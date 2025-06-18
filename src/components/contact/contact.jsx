cd55d142afa6849408f9db9a2bbfbae42c944b07
Hillary Onyango <hillaryonyango19@gmail.com>
2025-06-18 22:31:46 +0300

    Fix react-scripts version

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -39,12 +39,23 @@ const Contact = () => {
                     <button type='submit' value='Send' className='submitBtn'>Submit</button>
                 </form>
 
-
                 <div className='social-icons'>
-                    <a href='https://linkedin.com' target='_blank' rel='noopener noreferrer'><i className='fab fa-linkedin-in'></i></a>
-                    <a href='https://github.com/HillaryOnyango' target='_blank' rel='noopener noreferrer'><i className='fab fa-github'></i></a>
-                    <a href='https://x.com/amolo_hillary' target='_blank' rel='noopener noreferrer'><i className='fab fa-x-twitter'></i></a>
-                    <a href='mailto:hillaryonyango19@gmail.com@gmail.com'><i className='fas fa-envelope'></i></a>
+                    <a href='https://linkedin.com' target='_blank' rel='noopener noreferrer'>
+                        <i className='fab fa-linkedin-in'></i>
+                    </a>
+                    <a href='https://github.com/HillaryOnyango' target='_blank' rel='noopener noreferrer'>
+                        <i className='fab fa-github'></i>
+                    </a>
+                    <a href='https://x.com/amolo_hillary' target='_blank' rel='noopener noreferrer'>
+                        <i className='fab fa-x-twitter'></i>
+                    </a>
+                    <a
+                        href='https://mail.google.com/mail/?view=cm&fs=1&to=hillaryonyango19@gmail.com'
+                        target='_blank'
+                        rel='noopener noreferrer'
+                    >
+                        <i className='fas fa-envelope'></i>
+                    </a>
                 </div>
             </div>
         </div>
